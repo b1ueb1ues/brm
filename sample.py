e52797599c0c15eda82a6ee06f36e7a672913c6c
b1ueb1ues <bblues@live.cn>
2017-01-17 15:27:52 +0800

    a more complicated sample

--- sample.py
+++ sample.py
@@ -1,5 +1,20 @@
 from brm import *
-a = brm(haste=1.25,equip=['wrist'],talent=['black','bc'],mastery = 0.3)
+
+a = brm(haste = 1.3, equip=[''], mastery = 0.3, meleetakeiv = 1.5)
 a.run(100000)
-a.showavoid()
+atake = 1-a.showavoid()
+print 'brew %d + 3*%d'%(a.brewgot, a.blackgot)
+
+print '------'
+
+b = brm(haste = 1.3, equip=['wrist','waist'], mastery = 0.3, meleetakeiv = 1.5)
+b.run(100000)
+btake = 1-b.showavoid()
+print 'brew %d + 3*%d'%(b.brewgot, b.blackgot)
+
+
+print '------'
+avoid = 1-btake/atake
+print 'wrist+waist = ',avoid
+
 
