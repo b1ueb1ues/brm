b73aee2b81c56a1f5cb0545d03f428041c65880e
b1ueb1ues <bblues@live.cn>
2017-05-05 19:27:43 +0800

    not complete

--- brmbase.py
+++ brmbase.py
@@ -3,223 +3,41 @@
 
 import random
 from event import *
+from config import *
 debug = 0
 
 def main():
-    b = brmbase(talent=['black','light','ht','bc','ed'],equip=['ring','waist','wrist'], \
-            iron = 8, palmcdr = 1.4, haste = 1.3, dodgebase = 0.08, mastery = 0, crit = 0, vers = 0 )
+    b = brmbase(talent=['black','light','ht','bc','ed'],equip=['ring','waist','wrist'], stat=[25,25,0,20],\
+            iduration = 8, palmcdr = 1.4, haste = 30, dodgebase = 0.08, mastery = 0, crit = 0, vers = 0 )
 
-    print brmbase.__dict__
+    print 'test'
+    b.palmcdr = 1.5
+    print 'end test'
 
     class test(brmbase):
         def run(this):
             print 'hello'
             print this.__dict__
 
-    t = test()
+    t = test(stat=[25,25,0,20])
     t.run()
 
-'''
-class Event:
-    time = -1
-    dst = -1
-    src = -1
-    el = -1
 
-    def addto(this, el):
-        el.add(this)
+class brmbase(object):
 
-    def __init__(this, src = -1, time=0, dst = 0):
-        this.time = time
-        this.src = src
-        this.dst = dst
-    def __repr__(this):
-        return this.__class__.__name__+" at %.2f"%this.time
-    def __str__(this):
-        return this.__class__.__name__+" at %.2f"%this.time
-
-    def move(this, offset = -1, newtiming = 0):
-        this.el.move(this, offset, newtiming)
-
-    def rm(this):
-        this.el.rm(this)
-
-    def elprocess(this):
-        this.process()
-
-    def process(this):
-        print this.time,'process'
-#}
-
-class RepeatEvent(Event):
-    repeat = -1
-
-    def __init__(this, src, repeat = -1,time=0.0, dst=0):
-        Event.__init__(this, src, time, dst)
-        if repeat != -1  :
-            this.repeat = repeat
-
-    def repeatproc(this):
-        print this.time,this,'repeatproc (deprecated)'
-
-    def elprocess(this):
-        this.process()
-        if this.repeat <= -1 :
-            return
-        this.time += this.repeat
-        this.el.add(this)
-
-    def process(this):
-        print this.time,'repeatev proc'
-
-
-#}class repeatevent
-
-
-
-class Eventlist:
-    time = -1
-    _list = []
-    brm = -1
-    def __str__(this):
-        return str(this._list)
-
-    def __init__(this):
-        this._list = []
-
-
-    def add(this,event):
-        event.el = this
-        if debug == 0:
-            print "%.2f"%this.time,'add',event
-        timing = event.time
-        for i in range(len(this._list)) :
-            if timing <= this._list[i].time  :
-                tmp = this._list[:i]
-                tmp.append(event)
-                tmp += this._list[i:]
-                this._list = tmp
-                return True
-        this._list.append(event)
-    #}
-    
-    def rm(this,event):
-        for i in range(len(this._list)) :
-            if this._list[i] == event :
-                ret = this._list.pop(i)
-                event.el = -1
-                return ret
-        print this.time, ': rm 403', event
-        print this
-        exit()
-        return -1
-
-
-    def move(this, event, offset = -2, newtiming = 0):
-        if newtiming == -1:
-            e = this.rm(event)
-            if e == -1 :
-                print this.time,': move 403', event
-                return 
-            e.time += offset
-           # if e.time < this.time :
-           #     e.time = this.time
-            this.add(e)
-        else :
-           # if newtiming < this.time :
-           #     newtiming = this.time
-            e = this.rm(event)
-            if e == -1 :
-                print this.time,': move 403', event
-                return 
-            e.time = newtiming
-            this.add(e)
-
-
-    def procone(this):
-        e = this._list.pop(-1)
-        if debug == 0:
-            print e,this._list
-        if this.time < e.time:
-            this.time = e.time
-        if debug == 0:
-            print '%.1f: '%this.time,e,this
-        e.elprocess()
-        return #this.time
-    
-
-    def run(this, time = 9999):
-        while(0):
-            if this.time > time :
-                return
-            this.procone()
-
-#} class eventlist
-'''
-
-class config():
-    count = [0]
-    def show(this):
-        print this.stat,this.equip,this.talent,this.iduration,this.palmcdr
-
-    def __init__(this,stat=0,equip=['4t'],talent=['black','ht15'],iduration=8.5, prate = 0.5,palmcdr=1.4, haste=0, crit=0, vers=0, mastery=0):
-        if stat == 0 :
-            stat = [25,25,0,27]
-        if len(stat)!= 4:
-            print 'stat err'
-            exit()
-        this.stat = stat
-        this.crit = float(stat[0])/100
-        this.haste = float(stat[1])/100+1
-        this.vers = float(stat[2])/100
-        this.mastery = float(stat[3])/100
-        this.equip = equip
-        this.talent = talent
-        this.iduration = iduration
-        this.palmcdr = palmcdr
-        this.prate = prate
-
-        if crit != 0 :
-            this.crit = crit
-            this.stat[0] = int(crit*100)
-        if haste != 0 :
-            this.haste = haste
-            this.stat[1] = int(haste*100-100)
-        if vers != 0 :
-            this.vers = vers
-            this.stat[2] = int(vers*100)
-        if mastery != 0 :
-            this.mastery = mastery
-            this.stat[3] = int(mastery*100)
-
-    haste = 0
-    crit = 0
-    mastery = 0
-    vers = 0
-    stat = []
-
-    palmcdr = 0
-    iduration = 0
-    prate = 0
-
-    equip=[]
-    talent=[]
-
-
-class brmbase:
     #ed : elusive dance
     #ht : high tolerance
     #bc : blackout combo
 
 
     fout = 0
-
     el = 0
+    conf = 0
+    init = 0
 
     talent = []
     equip = []
     stat = []
-    conf = 0
 
     ring = 0
     waist = 0
@@ -353,34 +171,28 @@ class brmbase:
             this.takestdmg()
 
 
-    def __init__(this,conf=0,talent=['black','ht'],equip=['ring','waist'], \
-            iduration = 8, prate=0.5, palmcdr = 1.4, haste = 1.3, dodgebase = 0.10, mastery = 0, crit = 0, vers = 0 ):
+    def __setattr__(this,name,value):
+        if this.init :
+            tmpconf = this.conf
+            tmpconf.__setattr__(name,value)
+            tmp = this.__class__(tmpconf)
+            print tmp
+            this = tmp
+            exit()
+            return
+
+        super.__setattr__(this,name,value)
+
+    def __init__(this,**argv):
+        this.conf = config(dic = argv)
+        this.conf.setup(this)
+
         random.seed(1)
+
 
 
         this.el = Eventlist()
         this.el.src = this
-
-        if conf == 0 :
-            tmpstat = [int(crit*100),int(haste*100-100),int(vers*100),int(mastery*100)]
-            conf = config(stat=tmpstat, equip = equip, talent = talent, palmcdr = palmcdr, iduration = iduration, prate = prate)
-
-        this.conf = conf
-
-        this.dodgebase = dodgebase
-        this.kegcdr = 4
-
-        this.iduration = conf.iduration
-        this.prate = conf.prate
-        this.mastery = conf.mastery
-        this.haste = conf.haste
-        this.crit = conf.crit
-        this.vers = conf.vers
-        this.palmcdr = conf.palmcdr
-        this.talent = conf.talent
-        this.equip = conf.equip
-        this.stat = conf.stat
-
 
         for t in this.talent:
             if t == 'ht' or t == 'ht1' or t == 'ht10':
@@ -420,6 +232,7 @@ class brmbase:
                 this.wrist =1
 
         this.brewcd /= this.haste
+        this.init = 1
 
 
     #}init
--- config.py
+++ config.py
@@ -1,3 +1,104 @@
-class config():
-    def __init__(this):
-        pass
+class config(object):
+
+    dic = 0
+    stat = [0,0,0,0]
+
+    def __init__(this, **argv):
+        for a in argv:
+            this.__setattr__(a,argv[a])
+        if this.dic != 0:
+            dic = this.dic
+            tmpstat = [-1,-1,-1,-1]
+            for i in dic :
+                if i == 'crit':
+                    tmpstat[0] = dic[i]
+                elif i == 'haste':
+                    tmpstat[1] = dic[i]
+                elif i == 'vers':
+                    tmpstat[2] = dic[i]
+                elif i == 'mastery':
+                    tmpstat[3] = dic[i]
+                else:
+                    this.__setattr__(i,dic[i])
+            for i in range(4):
+                if tmpstat[i] != -1 :
+                    this.stat[i] = tmpstat[i]
+
+
+        this.__delattr__('dic')
+
+
+
+    def __setattr__(this,name,value):
+        if name == 'crit' :
+            this.stat[0] = value
+        elif name == 'haste' :
+            this.stat[1] = value
+        elif name == 'vers' :
+            this.stat[2] = value
+        elif name == 'mastery' :
+            this.stat[3] = value
+        elif name == 'stat' :
+            this.crit = value[0]
+            this.haste = value[1]
+            this.vers = value[2]
+            this.mastery = value[3]
+            
+        super.__setattr__(this,name,value)
+
+    def setup(this,target):
+        dic = this.__dict__
+        for i in dic :
+            if i == 'haste' :
+                h = float(dic[i])
+                target.__setattr__(i,h/100+1)
+            elif i == 'crit' :
+                c = float(dic[i])
+                target.__setattr__(i,c/100)
+            elif i == 'vers' :
+                v = float(dic[i])
+                target.__setattr__(i,v/100)
+            elif i == 'mastery' :
+                m = float(dic[i])
+                target.__setattr__(i,m/100)
+            else:
+                target.__setattr__(i,dic[i])
+
+
+    def show(this):
+        dic = this.__dict__
+        print '--------------------'
+        for i in dic :
+            if i == 'stat' :
+                print 'stat: [%d, %d, %d/%.0f, %d]'%(dic[i][0],dic[i][1],dic[i][2],dic[i][2]/2,dic[i][3])
+            elif i == 'crit' :
+                pass
+            elif i == 'haste' :
+                pass
+            elif i == 'vers' :
+                pass
+            elif i == 'mastery' :
+                pass
+            else:
+                print '%s: %s'%(i,dic[i])
+
+        print '--------------------'
+
+
+def main():
+
+
+    a = {'equip':['4t'],
+        'talent':['black','ht15'],
+        'isb':8.5,
+        'haste':30,
+        'stat':[30,20,11,25]
+        }
+    c = config(dic=a,test=4)
+    c.haste = 30
+    c.show()
+    c.stat = [25,25,11,25]
+    c.show()
+
+if __name__ == "__main__" :
+    main()
