2325fb71aec99a9bbf73c243e72ba48ebe8918a7
b1ueb1ues <bblues@live.cn>
2018-06-19 15:18:27 +0800

    prerelease

--- md2nga.py
+++ md2nga.py
@@ -8,35 +8,52 @@ from markdown2 import main as mmain
 
 
 def processall(a):
+    alt = 1
     print '-------'
     replace = {
-        '<p>'   : '', '</p>'   : '',
-        '<pre>' : '', '</pre>' : '',
-
-        '<h1>'         : '[h][size=150%]' , '</h1>'         : '[/size][/h]' , # #
-        '<h2>'         : ''               , '</h2>'         : ''            , # ##
-        '<h3>'         : ''               , '</h3>'         : ''            , # ###
-        '<h4>'         : ''               , '</h4>'         : ''            , # ####
-        '<h5>'         : ''               , '</h5>'         : ''            , # #####
-        '<h6>'         : ''               , '</h6>'         : ''            , # ######
-        '<strong>'     : '[b][size=110%]' , '</strong>'     : '[/b][/size]' , # **
-        '<em>'         : '[color=red]'    , '</em>'         : '[/color]'    , # *
-        '<blockquote>' : '[quote]'        , '</blockquote>' : '[/quote]'    , # >
-        '<code>'       : '[code]'         , '</code>'       : '[/code]'     , # `
-        '<ul>'         : '[list]'         , '</ul>'         : '[/list]'     , # -
-        '<li>'         : '[*]'            , '</li>'         : ''            , # -
-        '<table>'      : '[table]'        , '</table>'      : '[/table]'    ,
+        '<p>'   : ''        , '</p>'   : ''         ,
+       #'<p>'   : '[quote]' , '</p>'   : '[/quote]' ,
+        '<pre>' : ''        , '</pre>' : ''         ,
+       #-------------
+        '<h1>' : '[h][b][size=150%]' , '</h1>' : '[/size][/b][/h]' , # #
+        '<h2>' : '[h][b][size=130%]' , '</h2>' : '[/size][/b][/h]' , # ##
+        '<h3>' : '[b][size=120%]'    , '</h3>' : '[/size][/b]'     , # ###
+        '<h4>' : '[b][size=110%]'    , '</h4>' : '[/size][/b]'     , # ####
+        '<h5>' : '[b][size=100%]'    , '</h5>' : '[/size][/b]'     , # #####
+        '<h6>' : '[b][size=100%]'    , '</h6>' : '[/size][/b]'     , # ######
+       # # ## ### #### ##### ######
+        '<ul>'         : '[list]'         , '</ul>'         : '[/list]'     , # list
+        '<ol>'         : '[list]'         , '</ol>'         : '[/list]'     , # list
+        '<li>'         : '[*]'            , '</li>'         : ''            ,
+       #-------------
+        '<table>'      : '[table]'        , '</table>'      : '[/table]'    , # table
+        '<thead>'      : ''               , '</thead>'      : ''            ,
+        '<th>'         : '[th]'           , '</th>'         : '[/th]'       ,
+        '<tbody>'      : ''               , '</tbody>'      : ''            ,
         '<tr>'         : '[tr]'           , '</tr>'         : '[/tr]'       ,
         '<td>'         : '[td]'           , '</td>'         : '[/td]'       ,
+       #-------------
+        '<strong>'     : '[b][size=110%]' , '</strong>'     : '[/size][/b]' , # **
+        '<em>'         : '[color=red]'    , '</em>'         : '[/color]'    , # *
+        '<blockquote>' : '[quote]'        , '</blockquote>' : '[/quote]'    , # >
+       #'<blockquote>' : '[collapse]'     , '</blockquote>' : '[/collapse]' , # >
+        '<code>'       : '[code]'         , '</code>'       : '[/code]'     , # `
+        '<strike>'     : '[del]'          , '</strike>'     : '[/del]'      , # ~~
+        '<hr />'       : '======'         , '<hr>'          : ''            , # ---
 
         '__END__' : '__END__'
         }
+
+    if alt :
+        replace['<blockquote>'] = '[collapse]'
+        replace['</blockquote>'] = '[/collapse]'
+        replace['<p>'] = '[quote]'
+        replace['</p>'] = '[/quote]'
+
     for i in replace:
         a = a.replace(i,replace[i])
     print a
 
-test = {
-        }
 
 def processline(l):
     pass
@@ -44,11 +61,18 @@ def processline(l):
 def main():
     ext = {
             "tables":1,
-            #"fenced-code-blocks":1,
+            "fenced-code-blocks":1,
+            "numbering":1,
+            #"spoiler":1,
+            "strike":1,
             "__END__":0
     }
-    a = mmain(path='b.md',extras=ext)
-    print a
+    if len(sys.argv) >= 2:
+        if sys.argv[1] :
+            path = sys.argv[1]
+    else:
+        path = 'b.md'
+    a = mmain(path=path,extras=ext)
     processall(a)
 
 if __name__ == "__main__":
