cb6ae279c6bff5f281ea6de66c98c0548aeba335
b1ueb1ues <bblues@live.cn>
2018-07-05 17:24:15 +0800

    timeline complete brmbase progress

--- brmbfa/brmbase.py
+++ brmbfa/brmbase.py
@@ -1,3 +1,5 @@
+from timeline import Timeline
+from timeline import Processor
 
 class Skill(object):
     pass
@@ -19,6 +21,53 @@ class Aura(object):
 
 class Brmbase(object):
     def __init__(this):
-        pass
+        this.tl = Timeline()
+        this.gcd = this.timer(this.p_gcd, (0, 0), 0)
+        this.gcd.idle = 1
+        this.ks = this.timer(this.p_ks, (0, 6.5), 0)
+        this.ks.cd = 0
     
+    def timer(this, cb, ti=(0, 0), enable=1):
+        timing = ti[0]
+        interval = ti[1]
+        if not timing and interval:
+            timer = this.tl.createp(cb,this.tl.now(),interval)
+        else:
+            timer = this.tl.createp(cb,timing,interval)
+        if enable:
+            timer.enable()
+        return timer
 
+    def p_gcd(this, p):
+        if this.act_gcd() :
+            this.idle = 0
+            this.gcd.set(0,1)
+        else:
+            this.idle = 1
+            this.gcd.set(0,0)
+
+    def act_gcd(this):
+        if not this.ks.cd :
+            print 'ks!', this.tl.now()
+            this.ks.cd = 1
+            this.ks.enable()
+            return 1
+        else:
+            return 0
+
+    def p_ks(this, p):
+        p.cd = 0
+        p.disable()
+
+
+    def run(this):
+        this.gcd.enable()
+        this.tl.run(50,0.1)
+
+def main():
+    brm = Brmbase()
+    brm.run()
+
+
+if __name__ == "__main__":
+    main()
--- brmbfa/timeline.py
+++ brmbfa/timeline.py
@@ -1,44 +1,63 @@
 class Processor(object):
-    def __init__(this, timing=0, period=0, func=0):
-        this.timing = int(timing * 1000)
-        this.period = int(period * 1000)
-        if not timing and not period :
-            this.period = 1
+    def __init__(this, cb=0, timing=0, interval=0):
+        this.handler = -1
+        this.timeline = Timeline()
+        this.set(timing, interval, cb)
 
-        if func:
-            this.register(func)
+
+    def set(this, timing=0, interval=0, cb=0):
+        this.timing = int(timing * 1000)
+        this.interval = int(interval * 1000)
+
+        if not timing :
+            if not interval :
+                this.interval = 1
+            if this.interval > 1:
+                this.timing = this.timeline._now
+
+        if cb:
+            this.register(cb)
         else:
             this.register(this._process)
 
-    def register(this, func):
-        this._process = func
-        if this.timing :
-            this.process = this._check
-        else:
-            this.process = this._direct
 
-    def disable(this):
-        def nop(timing):
-            return
-        this.process = nop
+    def now(this):
+        if this.timeline:
+            return this.timeline.now()
+
 
     def enable(this):
-        if this.timing :
-            this.process = this._check
+        if this.interval > 1:
+            this.timing = this.timeline._now
+        this.timeline.add(this)
+        return this
+
+
+    def disable(this):
+        this.timeline.rm(this)
+        return this
+
+
+    def register(this, cb):
+        this._process = cb
+        if not this.timing and this.interval==1:
+            this.process = this._direct
         else:
-            this.process = this._process
+            this.process = this._check
 
 
     def _check(this, timing):
-        if this.period :
-            if (timing - this.timing) % this.period == 0:
-                this._process(this,timing)
-        else :
+        if this.interval : # repeat
+            if (timing - this.timing) % this.interval == 0:
+                this._process(this)
+        else : # proc once
             if (timing - this.timing) == 0:
-                this._process(this,timing)
+                this._process(this)
+                this.disable()
+
 
     def _direct(this, timing):
-        this._process(timing)
+        this._process(this)
 
 
     @staticmethod
@@ -46,15 +65,30 @@ class Processor(object):
         # sample plain _process
         if not this.timing :
             print '-- plain tick processor @', timing
-        elif this.period :
-            print '-- plain period processor per %d @ %d'%(this.period, timing)
+        elif this.interval :
+            print '-- plain interval processor per %d @ %d'%(this.interval, timing)
         else :
             print '-- plain once processor @', timing
+
 
     def process(): # virtual
         "this is the main process for outer to call"
         "but it is actually no need to def hear"
         "just for reference"
+
+#{{{
+    def _disable(this):
+        def nop(timing):
+            return
+        this.process = nop
+
+    def _enable(this):
+        if this.timing :
+            this.process = this._check
+        else:
+            this.process = this._process
+#}}}
+#} class Processor
 
 
 class Timeline(object):
@@ -63,57 +97,91 @@ class Timeline(object):
         this._now = 0
         this.nextid = 0
         this.processors = {}
-        this.disabled = {}
+        #this.disabled = {}
+        this.p2add = []
+        this.h2rm = []
         pass
 
-    def run(this,time=10):
+    def run(this, time=10, interval = 0.001):
+        _interval = int(interval * 1000)
         _time = int(time * 1000)
-        for i in range(_time):
-            this.process()
-            this._now += 1
+        i = 0 - _interval
+        while 1:
+            i += _interval
+            if i >= _time:
+                break
+            this._now = i
+            #this.now += 1
+            this.process(i)
+
+
+    def createp(this, cb, timing=0, interval=0):
+        p = Processor(cb,timing,interval)
+        p.timeline = this
+        return p
+
+    def now(this):
+        return this._now / 1000.0
+
 
     def add(this, p):
         handler = this.nextid
         this.nextid += 1
-        this.processors[handler] = p
+        p.handler = handler 
+        p.timeline = this
+        this.p2add.append(p)
         return handler
-    def rm(this, handler):
-        this.processors.pop(handler)
+
+    def rm(this, p):
+        this.h2rm.append(p.handler)
+
+    def _async_add(this):
+        for i in this.p2add:
+            this.processors[i.handler] = i
+        this.p2add = []
 
 
-    def process(this):
-        for i in this.enabled:
-            enabled[i].process(this._now)
+    def _async_rm(this):
+        for i in this.h2rm:
+            this.processors.pop(i)
+        this.h2rm = []
+
+
+    def process(this, timing):
+        this._async_rm()
+        this._async_add()
+        for i in this.processors:
+            p = this.processors[i]
+            p.process(timing)
 
 class test(object):
     def __init__(this):
         this.p = Processor()
         this.p.register(this.foo)
-    def foo(this, timing):
-        print '++ class tick processor @', timing
+    def foo(this, proc):
+        print '++ class tick processor @', proc.now(), proc.timing, proc.interval
 
 
 def main():
-    def b(this, timing):
-        print '-- modified period processor @', timing, this.timing, this.period
-        if timing >= 50:
-            this.disable()
-    def c(this, timing):
-        print '-- modified once processor @', timing, this.timing, this.period
-        this.pp.enable()
+    def b(p):
+        print '-- modified interval processor @', p.now(), p.timing, p.interval
+        if p.now() >= 0.05:
+            p.disable()
+    def c(p):
+        print '-- modified once processor @', p.now(), p.timing, p.interval
+        p.pp.enable()
+
+    t = Timeline()
 
     ob = test()
 
-    pp = Processor(0.01,0.01)
-    pp.register(b) 
-    po = Processor(0.07,0)
-    po.register(c)
+    pp = t.createp(b, 0.01,0.01)
+    po = t.createp(c, 0.07,0)
     po.pp = pp
 
-    t = Timeline()
-    t.processor.append(ob.p)
-    t.processor.append(pp)
-    t.processor.append(po)
+    t.add(ob.p)
+    #t.add(pp)
+    #t.add(po)
     t.run(0.1)
 
 if __name__ == "__main__" :
